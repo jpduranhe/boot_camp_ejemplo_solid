36c63912bbcf240549463ee1526f30e5fe8db3d0
Juan Pablo Duran <pablo_duran@unicomer.com>
2024-04-12 21:32:02 -0400

    lo realizao

--- src/solid/correcion/implementacion_l/Runner.java
+++ src/solid/correcion/implementacion_l/Runner.java
@@ -8,10 +8,18 @@ import static solid.correcion.ConstantesPrograma.IVA;
 import solid.Libro;
 import solid.correcion.DbLibro;
 import solid.correcion.Factura;
+import solid.correcion.implementacion_o.impresion.FacturaImpresionConsola;
 import solid.correcion.implementacion_o.impresion.FacturaImpresionLaser;
+import solid.correcion.implementacion_o.impresion.ImprimeFactura;
+import solid.correcion.implementacion_o.persistencia.FacturaPersistenciaArchivo;
 import solid.correcion.implementacion_o.persistencia.FacturaPersistenciaMysql;
+import solid.correcion.implementacion_o.persistencia.PersistirFactura;
 
 public class Runner {
+	
+	
+	private static ImprimeFactura imprimeFactura;
+	private static PersistirFactura persistirFactura;
 	
 	public static void main(String[] args) {
 		
@@ -19,27 +27,30 @@ public class Runner {
 		Libro[] listadoLibrosDisponibles=objDbLibro.librosDispobibles();
 		Libro harryPotterI=listadoLibrosDisponibles[0];
 		Libro harryPotterII=listadoLibrosDisponibles[1];
-		Libro harryPotterIII=listadoLibrosDisponibles[2];
+		Libro harryPotterIII=listadoLibrosDisponibles[2];		
+		
+		persistirFactura =  new FacturaPersistenciaArchivo();
 		
 		facturar(harryPotterI,2,DESCUENTO_10_PORCIENTO);
 		facturar(harryPotterII,1,DESCUENTO_20_PORCIENTO);
 		facturar(harryPotterIII,3,DESCUENTO_30_PORCIENTO);
 
+	}	
+	
+	private static void facturar(Libro libro,int cantidad,double descuento) {		
+		
+		Factura factura = new Factura(libro,cantidad,descuento,IVA);		
+		imprimeFactura = new FacturaImpresionConsola(factura);
+		imprimir(factura);
+		
+		persistir(factura);
 	}
-	
-	
-	private static void facturar(Libro libro,int cantidad,double descuento) {
-		
-		
-		Factura factura = new Factura(libro,cantidad,descuento,IVA);
-		
-		FacturaImpresionLaser facturaImpresion = new FacturaImpresionLaser(factura);
-		FacturaPersistenciaMysql facturaPersistencia =  new FacturaPersistenciaMysql();
-		facturaImpresion.imprimir();
-		
-		facturaPersistencia.guardar(factura);
+	private static void imprimir(Factura factura) {
+		imprimeFactura.imprimir();
 	}
-	
+	private static void persistir(Factura factura) {
+		persistirFactura.guardar(factura);
+	}
 	
 
 }
